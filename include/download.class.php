2e28f747d4d49ff2453eb86b1f2904304f7b35dc
dengenxp <taharaxp@gmail.com>
2017-05-04 23:18:20 +0900

    Fixed a bug where no error message when uploading of file fails (bug #5)

--- include/download.class.php
+++ include/download.class.php
@@ -888,6 +888,24 @@ class DLDownload
         if (empty($this->_lid)) $this->_lid = $this->_old_lid;
 
         // Validate the input values ----------------------->
+        if (!isset($_FILES['newfile']['error']) || !is_int($_FILES['newfile']['error'])) {
+            $this->_errno[] = '1401';
+        } else {
+            switch ($_FILES['newfile']['error']) {
+                case UPLOAD_ERR_OK:
+                    break;
+                case UPLOAD_ERR_NO_FILE:
+                    $this->_errno[] = '1402';
+                    break;
+                case UPLOAD_ERR_FORM_SIZE:
+                case UPLOAD_ERR_INI_SIZE:
+                    $this->_errno[] = '1403';
+                    break;
+                default:
+                    $this->_errno[] = '1404';
+                    break;
+            }
+        }
         if (empty($this->_title)) {
             $this->_errno[] = '1101';
         }
@@ -1125,6 +1143,21 @@ class DLDownload
         $newfile_name = $_FILES['newfile']['name'];
 
         // Validate the input values ----------------------->
+        if (isset($_FILES['newfile']['error']) && is_int($_FILES['newfile']['error'])) {
+            switch ($_FILES['newfile']['error']) {
+                case UPLOAD_ERR_OK:
+                    break;
+                case UPLOAD_ERR_NO_FILE:
+                    break;
+                case UPLOAD_ERR_FORM_SIZE:
+                case UPLOAD_ERR_INI_SIZE:
+                    $this->_errno[] = '1403';
+                    break;
+                default:
+                    $this->_errno[] = '1404';
+                    break;
+            }
+        }
         if (empty($this->_title)) {
             $this->_errno[] = '1101';
         }
@@ -1453,6 +1486,24 @@ class DLDownload
         if (empty($this->_lid)) $this->_lid = $this->_old_lid;
 
         // Validate the input values ----------------------->
+        if (!isset($_FILES['newfile']['error']) || !is_int($_FILES['newfile']['error'])) {
+            $this->_errno[] = '1401';
+        } else {
+            switch ($_FILES['newfile']['error']) {
+                case UPLOAD_ERR_OK:
+                    break;
+                case UPLOAD_ERR_NO_FILE:
+                    $this->_errno[] = '1402';
+                    break;
+                case UPLOAD_ERR_FORM_SIZE:
+                case UPLOAD_ERR_INI_SIZE:
+                    $this->_errno[] = '1403';
+                    break;
+                default:
+                    $this->_errno[] = '1404';
+                    break;
+            }
+        }
         if (empty($this->_title)) {
             $this->_errno[] = '1101';
         }
